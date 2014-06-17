0ec5d52f2c93e19281acfd3deb850290f101f279
Derek <tycoonbob@gmail.com>
2014-06-17 14:04:28 -0400

    serverconfig

--- intranet/serverconfig-example.php
+++ intranet/serverconfig-example.php
@@ -1,4 +1,6 @@
 <?php 
+
+  
 	$config = array(
 
 		# Services
@@ -11,34 +13,38 @@
 		"transmission" => false,
 
 		# URLs and Ports
-		"sickbeardURL" => "192.168.1.1",
+		"sickbeardURL" => "127.0.0.1",
 		"sickbeardPort" => "8081",
 		"sickbeardAPI" => "",
 		"sickbeardHTTPS" => false,
-		"sabnzbdURL" => "192.168.1.1",
+		"sabnzbdURL" => "127.0.0.1",
 		"sabnzbdPort" => "8080",
 		"sabnzbdAPI" => "",
 		"sabnzbdHTTPS" => false,
-		"couchpotatoURL" => "192.168.1.1",
+		"couchpotatoURL" => "127.0.0.1",
 		"couchpotatoPort" => "5000",
 		"couchpotatoHTTPS" => false,
-		"headphonesURL" => "192.168.1.1",
+		"headphonesURL" => "127.0.0.1",
 		"headphonesPort" => "8181",
 		"headphonesHTTPS" => false,
-		"uTorrentURL" => "192.168.1.1",
-		"uTorrentPort" => "8089",
-		"transmissionURL" => "192.168.1.1",
+		"uTorrentURL" => "127.0.0.1",
+		"uTorrentPort" => "8080",
+		"transmissionURL" => "127.0.0.1",
 		"transmissionPort" => "9091",
+		"observiumURL" => "127.0.0.1",
+		"observiumDBuser" => "root",
+		"observiumDBpass" => "foo",
+		"observiumDBname" => "observium",
 
 		# Usernames and Passwords
 		# If not using usernames or passwords, leave these to false.
 		# ie. "sickbeardUsername" => false,
 		"sickbeardUsername" => false,
 		"sickbeardPassword" => false,
-		"sabnzbdUsername" => "admin",
-		"sabnzbdPassword" => "admin",
-		"uTorrentUsername" => "admin",
-		"uTorrentPassword" => "admin",
+		"sabnzbdUsername" => false,
+		"sabnzbdPassword" => false,
+		"uTorrentUsername" => false,
+		"uTorrentPassword" => false,
 		"transmissionUsername" => false,
 		"transmissionPassword" => false,
 
@@ -47,7 +53,7 @@
 		# air the day after they say they're going to air, so instead of "coming shows", we use "missed shows"
 		# to indicate what's coming out today. 
 		# Set to true for "missed", false for "coming"
-		"sickMissed" => true,
+		"sickMissed" => false,
 
 		# Show popups when hovering over coming shows?
 		"sickPopups" => true,
@@ -56,21 +62,32 @@
 		"debug" => false,
 
 		# Show trailers button
-		"showTrailers" => true,
+		"showTrailers" => false,
 
 		# Wifi
 		# WifiName is also used for page title
 		"showWifi" => true,
-		"wifiName" => "Home Network",
-		"wifiPassword" => "abcd1234",
+		"wifiName" => "Internets",
+		"wifiPassword" => "Myp@55word",
 
 		# Bookmarks
 		"bookmarks" => array(
 			0 => array(
-				"label" => "NZBMatrix",
-				"url" => "http://www.nzbmatrix.com",
+				"label" => "Miatrix",
+				"url" => "https://www.miatrix.com",
+				"icon" => "intranet/images/miatrix.ico",
 			),
 			1 => array(
+				"label" => "NZBNDX",
+				"url" => "https://www.nzbndx.com/",
+				"icon" => "intranet/images/nzbndx.ico",
+			),
+      2 => array(
+				"label" => "NZBPlanet",
+				"url" => "https://www.nzbplanet.net/",
+				"icon" => "intranet/images/nzbplanet.ico",
+			),			
+			3 => array(
 				"label" => "Season Start Dates",
 				"url" => "intranet/comingseasons.php",
 				"icon" => "intranet/images/tv.png",
@@ -78,4 +95,22 @@
 		),
 
 	);
+	
+  $observiumcon=mysqli_connect("$config['observiumURL']","$config['observiumDBuser']","$config['observiumDBpass']","$config['observiumDBname']");
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  
+  $rebootedquery = "SELECT
+            hostname,
+            type,
+            os,
+            TIME_FORMAT(SEC_TO_TIME(uptime),'%Hh %im') AS Uptime,
+            last_polled AS 'Last SNMP Poll'
+          FROM
+            devices
+          WHERE
+            uptime < 86400";
+
 ?>
